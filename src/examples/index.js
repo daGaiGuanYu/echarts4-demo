8870755af1bafacd9d2f7ae9a7f3eb02b21a2a36
韦现法 <372301467@qq.com>
2019-08-15 14:59:44 +0800

    line

--- src/examples/index.js
+++ src/examples/index.js
@@ -1,7 +1,11 @@
 import lineOption1 from './line/option1';
+import lineOption2 from './line/option2';
+import lineOption3 from './line/option3';
 
 const list = [];
 list.push(lineOption1);
+list.push(lineOption2);
+list.push(lineOption3);
 
 const result = {};
 list.forEach( item => {
--- src/examples/line/option3.js
+++ src/examples/line/option3.js
@@ -1,6 +1,6 @@
 export default {
   typeName: '折线图',
-  name: '平滑折线图',
+  name: '双折线图',
   option: {
     xAxis: {
       data: ["衬衫","羊毛衫","雪纺衫","裤子","高跟鞋","袜子"]
@@ -8,8 +8,11 @@ export default {
     yAxis: {},
     series: [{
       type: 'line',
-      smooth: true,
       data: [5, 20, 36, 10, 10, 20]
+    },{
+      type: 'line',
+      smooth: true,
+      data: [1, 2, 6, 18, 30, 20]
     }]
   }
 }
